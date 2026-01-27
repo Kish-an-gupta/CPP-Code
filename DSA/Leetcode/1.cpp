bc96488ee8536cfc0e260ed2f1a95525bd0b545b
Kish-an-gupta <kishan93419@gmail.com>
2026-01-27 23:58:45 +0530

    leetcode_1

---
+++ DSA/Leetcode/1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> ans;
+        for(int i=0;i<nums.size(); i++){
+            int first = nums[i];
+            int second = target - first;
+            if(m.find(second) != m.end()){
+                //found
+                ans.push_back(i);
+                ans.push_back(m[second]);
+            }
+            m[first] = i;  //storing value in map if we do not get match 
+        }
+        return ans;
+    }
+};
