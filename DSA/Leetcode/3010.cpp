0f6c46444f8b7352807c3cdaca69e6ab6f50bb10
Kish-an-gupta <kishan93419@gmail.com>
2026-02-01 22:06:45 +0530

    leetcode-3010

---
+++ DSA/Leetcode/3010.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int n = nums.size();
+        int cost = nums[0];
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+
+        for(int i=1; i<n; i++){
+            if(nums[i] < min1){
+                min2 = min1;
+                min1 = nums[i];
+            } else if(nums[i] < min2) min2 = nums[i];
+        }
+        return cost + min1 + min2;
+    }
+};
