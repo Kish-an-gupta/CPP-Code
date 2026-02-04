ec744e6da2cb57797d530d55ea1f9ab53299d844
Kish-an-gupta <kishan93419@gmail.com>
2026-02-04 23:36:41 +0530

    leetcode-31

---
+++ DSA/Leetcode/31.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) {
+         int n = arr.size();
+        int gola_index = -1;
+
+        for(int i=n-1; i>0; i--){
+            if(arr[i] > arr[i-1]){
+                gola_index = i-1;
+                 break;
+            }
+        }
+
+        if(gola_index != -1){
+            int swap_index = gola_index;
+            for(int j=n-1; j>gola_index; j--){
+                if(arr[j] > arr[gola_index]){
+                    swap_index = j;
+                    break;
+                }
+             }
+             swap(arr[gola_index], arr[swap_index]);
+        }
+        reverse(arr.begin() + gola_index + 1, arr.end());
+        
+    }
+};
